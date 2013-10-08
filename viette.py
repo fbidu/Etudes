b6d65d206598f3bddd6574eda54abf7bb75d0160
Felipe Bidu <bidu.pub@gmail.com>
2013-10-08 18:24:37 -0300

    Postagem inicial

---
+++ viette.py
@@ -0,0 +1,10 @@
+# -*- encoding: latin-1 -*- 
+n = int(input("Digite o número de interações: "))
+i = 0
+x = 0
+pi = 2
+while i < n:
+    x = ((2 + x)**0.5)
+    pi = pi*(2/x)
+    i += 1
+print(pi)
