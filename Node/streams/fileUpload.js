cab2c0d6672c0bc696a77110e68b8efe013bb2d0
Felipe Bidu <bidu.pub@gmail.com>
2015-11-12 18:19:42 -0200

    Updating the file upload script

--- Node/streams/fileUpload.js
+++ Node/streams/fileUpload.js
@@ -1,7 +1,17 @@
 var fs = require('fs');
 var http = require('http');
 
+// Creating a server
 http.createServer(function(request, response) {
+    // Creating a Write Stream to save the file
     var upload = fs.createWriteStream('uploaded');
+
+    // Piping the request to the stream
     request.pipe(upload);
+
+    // Listening to the 'end' event on the request
+    request.on('end', function() {
+        // Writing that it was done
+        console.log('uploaded');
+    }
 }).listen(8080);
