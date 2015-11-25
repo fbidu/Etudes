69bd7e4fcc5b894028e7f8b486fb7d4aff325260
Felipe Bidu <bidu.pub@gmail.com>
2015-11-25 11:34:34 -0200

    Using pipes and progress indicator for file upload

--- Node/streams/fileUpload.js
+++ Node/streams/fileUpload.js
@@ -6,6 +6,22 @@ http.createServer(function(request, response) {
     // Creating a Write Stream to save the file
     var upload = fs.createWriteStream('uploaded');
 
+    // Getting the total size
+    var fileBytes = request.headers['content-length'];
+
+    // How many bytes were uploaded?
+    var uploadedBytes = 0;
+
+    // Updating the progress!
+    request.on('readable', function() {
+        var chunk = null;
+        while(null !== (chunk = request.read())) {
+            uploadedBytes += chunk.length;
+            var progress = (uploadedBytes / fileBytes) * 100;
+            response.write("Progress: " + parseInt(progress, 10) + "%\n");
+        }
+    });
+
     // Piping the request to the stream
     request.pipe(upload);
 
@@ -13,5 +29,5 @@ http.createServer(function(request, response) {
     request.on('end', function() {
         // Writing that it was done
         console.log('uploaded');
-    }
+    })
 }).listen(8080);
