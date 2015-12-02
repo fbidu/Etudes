d6b6edf167947924e75709d1a427de27183b6567
fbidu <bidu.pub@gmail.com>
2015-12-02 00:24:03 -0200

    Adding files for Express lesson

---
+++ Node/express/tweet.js
@@ -0,0 +1,29 @@
+// App that, given a twitter username, fetches the latest 10 tweets
+// and displays them
+
+var request = require('request');
+var url = require('url');
+var express = require('express');
+var config = require('config');
+
+var app = express();
+
+app.get('/tweets/:username', function(req, response){
+	var username = req.params.username;
+
+	options = {
+		protocol: "https:",
+		host: 'api.twitter.com',
+		pathname: '/1.1/statuses/user_timeline.json',
+		query: { screen_name: username, count: 10 }
+	}
+	oauth = {
+	   	consumer_key: config.get("Twitter.consumer_key"),
+		consumer_secret: config.get("Twitter.consumer_secret")
+	}
+
+	var twitterUrl = url.format(options);
+	request({url: twitterUrl, oauth:oauth}).pipe(response);
+});
+
+app.listen(8080);
