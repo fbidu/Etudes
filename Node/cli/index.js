d00cab0c0f362acc377aa0e677c87fc17bc60952
fbidu <bidu.pub@gmail.com>
2015-12-21 18:25:29 -0200

    Removing console log

--- Node/cli/index.js
+++ Node/cli/index.js
@@ -1,3 +1,11 @@
 #! /usr/bin/env node
 
-console.log('filesearch');
+var exec = require('child_process').exec;
+
+var userArgs = process.argv.slice(2);
+var searchPattern = userArgs[0];
+
+var child = exec('ls -a | grep ' + searchPattern, function(err, stdout, stderr)
+				 {
+					console.log(stdout);
+				 });
