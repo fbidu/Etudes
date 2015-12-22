677dfe821eae146502d6c65bac46ff78602a7890
fbidu <bidu.pub@gmail.com>
2015-12-22 02:08:26 -0200

    Creating virtuals and custom setters inside the Products schema

--- MEAN/edx/retail-api/products.js
+++ MEAN/edx/retail-api/products.js
@@ -15,5 +15,21 @@ var productSchema = {
 	category: Category.categorySchema
 };
 
-module.exports = new mongoose.Schema(productSchema);
+var schema = new mongoose.Schema(productSchema);
+
+var currencySymbols = {
+	'USD': '$',
+	'EUR': '€',
+	'GBP': '£'
+};
+
+schema.virtual('displayPrice').get(function() {
+	return currencySymbols[this.price.currency] +
+		'' + this.price.amount;
+});
+
+schema.set('toObject', {virtuals: true});
+schema.set('toJSON', {virtuals: true});
+
+module.exports = schema;
 module.exports.productSchema = productSchema;
