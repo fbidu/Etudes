e63ff8b7c0b689457ad745bf9b1d5111b528b0d5
fbidu <bidu.pub@gmail.com>
2015-12-22 02:14:32 -0200

    Adding internal field for estimated price in Products schema

--- MEAN/edx/retail-api/products.js
+++ MEAN/edx/retail-api/products.js
@@ -5,14 +5,30 @@ var productSchema = {
 	name: {type: String, required: true},
 	pictures: [{type: String, match:"http:\/\//i"}],
 	price: {
-		amount: {type: Number, required: true},
+		amount: {
+			type: Number,
+			required: true,
+			set: function(v) {
+				this.internal.approximatePriceUSD =
+					v / (fx()[this.price.currency] || 1);
+				return v;
+			}
+		},
 		currency: {
 			type: String,
 			enum: ['USD', 'EUR', 'GBP'],
-			required: true
+			required: true,
+			set: function(v) {
+				this.internal.approximatePriceUSD =
+					this.price.amount / (fx()[v] || 1);
+				return v;
+			}
 		}
 	},
-	category: Category.categorySchema
+	category: Category.categorySchema,
+	internal: {
+		approximatePriceUSD: Number
+	}
 };
 
 var schema = new mongoose.Schema(productSchema);
