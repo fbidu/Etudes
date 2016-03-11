037f64e4e856327cc7f77c1e48e9f92931243e2e
Felipe Bidu <felipe@felipevr.com>
2016-03-11 13:31:17 -0300

    Bump

---
+++ Python/django-rest/tutorial-serialization/tutorial/snippets/models.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted((
+# Create your models here.
