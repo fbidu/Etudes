016edcb3baa82f949d4f702225e9ff0facee1349
Felipe Bidu <felipe@felipevr.com>
2016-03-30 10:32:40 -0300

    Adding a more explicit conversion for JSON

--- MEAN/Express-Codeschool/hello-world/app.js
+++ MEAN/Express-Codeschool/hello-world/app.js
@@ -11,4 +11,10 @@ app.get('/blocks', function(request, response) {
     response.send(blocks);
 });
 
+// This function does the same as the above but it converts the array
+// to JSON in a more explict way
+app.get('/explicity_blocks', function(request, response) {
+    var blocks = ['fixed', 'movable', 'rotating'];
+    response.json(blocks);
+});
 app.listen(3000);
