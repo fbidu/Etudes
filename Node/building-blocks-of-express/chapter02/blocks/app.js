b19cbca477061413eb3d9d1d3b04d5e3c1a1acf3
Felipe Rodrigues <felipe@felipevr.com>
2016-05-21 13:12:34 -0300

    Adding files for Express

--- Node/building-blocks-of-express/chapter02/blocks/app.js
+++ Node/building-blocks-of-express/chapter02/blocks/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var logger = require('./logger');
 
+app.use(logger);
 app.use(express.static('public'))
 
 app.get('/blocks', function(request, response){
