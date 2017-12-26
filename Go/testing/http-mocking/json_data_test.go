a6d1fe26a6dfcd1a5619b4b94966729bde8fb1fd
Felipe Rodrigues <bidu.pub@gmail.com>
2017-12-26 14:57:22 -0200

    Adding test for json mocking

--- Go/testing/http-mocking/json_data_test.go
+++ Go/testing/http-mocking/json_data_test.go
@@ -1,6 +1,7 @@
-package always_teapot
+package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,16 +10,42 @@ import (
 const checkMark = "\u2713"
 const ballotX = "\u2717"
 
+var home = `{
+"name": "Alderaan",
+"rotation_period": "24",
+"orbital_period": "364",
+"diameter": "12500",
+"climate": "temperate",
+"gravity": "1 standard",
+"terrain": "grasslands, mountains",
+"surface_water": "40",
+"population": "2000000000",
+"residents": [
+	"https://swapi.co/api/people/5/",
+	"https://swapi.co/api/people/68/",
+	"https://swapi.co/api/people/81/"
+],
+"films": [
+	"https://swapi.co/api/films/6/",
+	"https://swapi.co/api/films/1/"
+],
+"created": "2014-12-10T11:35:48.479000Z",
+"edited": "2014-12-20T20:58:18.420000Z",
+"url": "https://swapi.co/api/planets/2/"
+}`
+
 func mockServer() *httptest.Server {
 	f := func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusTeapot)
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, home)
 	}
 
 	return httptest.NewServer(http.HandlerFunc(f))
 }
 
 func TestTeapot(t *testing.T) {
-	expectedStatus := http.StatusTeapot
+	expectedStatus := http.StatusOK
 
 	server := mockServer()
 	defer server.Close()
