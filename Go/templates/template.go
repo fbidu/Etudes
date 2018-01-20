c4bf1baacaf0af28c61cd3f146ea4a6b91467182
Felipe Rodrigues <bidu.pub@gmail.com>
2018-01-20 12:17:59 -0200

    Adding test for go templates

--- Go/templates/template.go
+++ Go/templates/template.go
@@ -10,16 +10,30 @@ type Todo struct {
 	Description string
 }
 
+type TodoPointer struct {
+	Name        *string
+	Description *string
+}
+
 func main() {
 	td := Todo{"Hello, templates", "What can you do?"}
 
-	t, err := template.New("todos").Parse("There is a task called {{.Name}}, with description {{.Description}}")
+	t, err := template.New("todos").Parse("There is a task called \"{{.Name}}\", with description \"{{.Description}}\"\n")
 
 	if err != nil {
 		panic(err)
 	}
 
 	err = t.Execute(os.Stdout, td)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var incompleteTd Todo
+	incompleteTd.Name = "Hi, this works, even missing a field"
+
+	err = t.Execute(os.Stdout, incompleteTd)
 
 	if err != nil {
 		panic(err)
