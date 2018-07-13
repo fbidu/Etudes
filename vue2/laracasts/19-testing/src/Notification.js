836a39fc45615c9bdb1f7ad58b126c61f7fde726
Felipe Rodrigues <felipe@felipevr.com>
2018-07-13 09:39:41 -0300

    Updating vue2 folder

--- vue2/laracasts/19-testing/src/Notification.js
+++ vue2/laracasts/19-testing/src/Notification.js
@@ -1,7 +1,4 @@
 export default {
-    data() {
-        return {
-            message: "Hello, world!"
-        };
-    }
+    template: "<div>{{ message }}</div>",
+    props: ['message'],
 };
--- vue2/laracasts/19-testing/test/notification.js
+++ vue2/laracasts/19-testing/test/notification.js
@@ -1,8 +1,23 @@
-import Vue from 'vue';
+import Vue from 'vue/dist/vue';
 import test from 'ava';
 
 import Notification from '../src/Notification';
 
+function notification(message) {
+    let n = Vue.extend(Notification);
+
+    let notif = new n({
+        propsData: {
+            message: message
+        }
+    }).$mount();
+
+    return notif
+}
 test("That it renders a notification", t => {
-    t.is(Notification.data().message, "Hello, world!");
+
+    notif = notification("Hello, world!");
+    let content = notif.$el.textContent;
+
+    t.is(content, "Hello, world!");
 });
