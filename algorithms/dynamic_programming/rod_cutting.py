36d63397359c6f4a9bb09cf0c665bfe6b8a67b39
Felipe V. Rodrigues <felipe@felipevr.com>
2018-11-28 09:49:31 -0200

    Does LRU caching help? Spoiler: it doesn't

--- algorithms/dynamic_programming/rod_cutting.py
+++ algorithms/dynamic_programming/rod_cutting.py
@@ -8,6 +8,7 @@ and Clifford Stein. 2009. Introduction to Algorithms,
 Third Edition(3rd ed.). The MIT Press.
 """
 
+from functools import lru_cache
 from math import inf
 from random import randint
 from time import perf_counter
@@ -30,6 +31,21 @@ def recursive_cutting(price_table, size):
         )
     return max_revenue
 
+@lru_cache(maxsize=1000)
+def recursive_cutting_lru_cached(price_table, size):
+    """
+    Given a price table p and size n, find the maximum
+    revenue possible
+    """
+    if size == 0:
+        return 0
+
+    max_revenue = -inf
+    for i in range(1, size + 1):
+        max_revenue = max(
+            max_revenue, price_table[i - 1] + recursive_cutting(price_table, size - i)
+        )
+    return max_revenue
 
 def main():
     """
@@ -38,8 +54,11 @@ def main():
     max_size = 22
     sample_pricing = [randint(1, 2000) for _ in range(40)]
 
+    print("Benchmarking recursive cutting with list of prices:")
     benchmark(recursive_cutting, [sample_pricing], range(1, max_size + 1))
 
-
+    print("Benchmarking recursive cutting with list of prices LRU Cached:")
+    sample_pricing = tuple(randint(1, 2000) for _ in range(40))
+    benchmark(recursive_cutting_lru_cached, [sample_pricing], range(1, max_size + 1))
 if __name__ == "__main__":
     main()
