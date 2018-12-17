c5081e569e68695f5448a6a8ed98eecdd6961cf3
Felipe V. Rodrigues <felipe@felipevr.com>
2018-12-17 10:33:32 -0200

    Simple Stagehand boilerplate

---
+++ dart/add-a-b/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:add_a_b/add_a_b.dart' as add_a_b;
+
+main(List<String> arguments) {
+  print('Hello world: ${add_a_b.calculate()}!');
+}
---
+++ dart/add-a-b/test/add_a_b_test.dart
@@ -0,0 +1,8 @@
+import 'package:add_a_b/add_a_b.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
