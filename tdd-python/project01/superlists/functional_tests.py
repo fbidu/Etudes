e25e707ab39e81510d52544b775760262a7dc9a6
Felipe V. Rodrigues <felipe@felipevr.com>
2019-02-19 18:04:10 -0300

    Function tests now checks for to-do item inputting

--- tdd-python/project01/superlists/functional_tests.py
+++ tdd-python/project01/superlists/functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -14,16 +16,27 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get("http://localhost:8000")
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test!")
+        self.assertIn("To-Do", self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", header_text)
 
         # She is invited to enter a to-do item straight away
-        #
+        input_box = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(input_box.get_attribute("placeholder"), "Enter a to-do item")
+
         # She types "Buy 00 Flour" into a text box
-        #
+        input_box.send_keys("Buy 00 Flour")
+
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy 00 Flour" as an item in a to-do list
-        #
+        input_box.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(any(row.text == "1: Buy 00 Flour" for row in rows))
+
+        self.fail("Finish the test!")
         # There is still a text box inviting her to add another item. She
         # enters "Buy canned tomatoes"
         #
@@ -36,6 +49,7 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there
         #
         # Satistfied, she goes back to sleep
+
 
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
