65415d8c40e7ee7c4e01538d0f3c882b7b87435d
Felipe V. Rodrigues <felipe@felipevr.com>
2019-02-19 17:31:56 -0300

    Add a first unit test and URL mapping with a dummy view!

--- tdd-python/project01/superlists/lists/tests.py
+++ tdd-python/project01/superlists/lists/tests.py
@@ -1,6 +1,9 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_homepage_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
