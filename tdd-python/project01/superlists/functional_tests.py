f4dd9c467f28df902c9c05125225e41edf253fc7
Felipe V. Rodrigues <felipe@felipevr.com>
2019-02-19 18:23:18 -0300

    Add more to home interface

--- tdd-python/project01/superlists/functional_tests.py
+++ tdd-python/project01/superlists/functional_tests.py
@@ -34,7 +34,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(any(row.text == "1: Buy 00 Flour" for row in rows))
+        self.assertTrue(
+            any(row.text == "1: Buy 00 Flour" for row in rows),
+            "New to-do item did not appear in table",
+        )
 
         self.fail("Finish the test!")
         # There is still a text box inviting her to add another item. She
