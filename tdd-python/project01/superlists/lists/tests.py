f4bdecdb3f51c503a11708e8cf85065adad4ab24
Felipe V. Rodrigues <felipe@felipevr.com>
2019-02-20 12:13:53 -0300

    Fix home page testing and add home POST testing
    We've add a CSRF token at the home page and it breaks the test
    because each time the page is rendered, a new CSRF token is generated
    so we are neve able to get the same results.
    
    This commit removes CSRF tokens from both the observed and the expected
    HTML in order to make a proper comparison. It also adds a new unit
    test to check if we're able to properly handle POST requests.
    
    Sources:
    https://www.reddit.com/r/learnpython/comments/3vjxzn/django_unit_testing_csrf_token_in_html_assertion/cy7432u

--- tdd-python/project01/superlists/lists/tests.py
+++ tdd-python/project01/superlists/lists/tests.py
@@ -1,5 +1,7 @@
+import re
+
 from django.http import HttpRequest
-from django.template.loader import render_to_string
+from django.shortcuts import render
 from django.test import TestCase
 from django.urls import resolve
 
@@ -12,8 +14,30 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
+        """
+        Tests if the home page actually render the home page
+        """
+        # Makes a request to our home
         request = HttpRequest()
         response = home_page(request)
 
-        expected_html = render_to_string("home.html")
-        self.assertEqual(response.content.decode(), expected_html)
+        # That request will include an CSRF token. That token changes with each
+        # request. We need to remove it in order to be able to test properly
+        csrf_regex = r"<input[^>]+csrfmiddlewaretoken[^>]+>"
+        observed_html = re.sub(csrf_regex, "", response.content.decode())
+
+        # We'll have the same problem while rendering the expected response.
+        expected_html = render(request, "home.html")
+        expected_html = re.sub(csrf_regex, "", expected_html.content.decode())
+
+        # Finally, we check everything
+        self.assertEqual(observed_html, expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST["item_text"] = "A new list item"
+
+        response = home_page(request)
+
+        self.assertIn("A new list item", response.content.decode())
