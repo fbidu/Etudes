f1de3ff290d4d8bc2751668cd92ee98afd967a88
Felipe V. Rodrigues <felipe@felipevr.com>
2019-02-20 20:02:19 -0300

    Refactors row text checking

--- tdd-python/project01/superlists/functional_tests.py
+++ tdd-python/project01/superlists/functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith heard about a cool new online to-do app. She checks the homepage.
         self.browser.get("http://localhost:8000")
@@ -33,9 +38,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy 00 Flour" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
         time.sleep(2)
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("1: Buy 00 Flour", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Buy 00 Flour")
 
         # There is still a text box inviting her to add another item. She
         # enters "Buy canned tomatoes"
@@ -45,9 +48,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(2)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn("2: Buy canned tomatoes", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Buy 00 Flour")
+        self.check_for_row_in_list_table("2: Buy canned tomatoes")
         
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
