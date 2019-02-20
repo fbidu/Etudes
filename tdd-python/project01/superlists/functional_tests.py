f370b83e10ab0ee5d62262ce5b82e85a496c4a4a
Felipe V. Rodrigues <felipe@felipevr.com>
2019-02-20 14:02:51 -0300

    Add more FTs

--- tdd-python/project01/superlists/functional_tests.py
+++ tdd-python/project01/superlists/functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -31,24 +32,27 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy 00 Flour" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
-
+        time.sleep(2)
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(
-            any(row.text == "1: Buy 00 Flour" for row in rows),
-            "New to-do item did not appear in table",
-        )
+        self.assertIn("1: Buy 00 Flour", [row.text for row in rows])
 
-        self.fail("Finish the test!")
         # There is still a text box inviting her to add another item. She
         # enters "Buy canned tomatoes"
-        #
+        input_box = self.browser.find_element_by_id("id_new_item")
+        input_box.send_keys("Buy canned tomatoes")
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(2)
+
         # The page updates again, and now shows both items on her list
-        #
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn("2: Buy canned tomatoes", [row.text for row in rows])
+        
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect
-        #
+        self.fail("Finish the test!")
         # She visits that URL - her to-do list is still there
         #
         # Satistfied, she goes back to sleep
