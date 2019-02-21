66875f0f179d3ba39357e02685bd78995b29fac6
Felipe V. Rodrigues <felipe@felipevr.com>
2019-02-21 09:23:08 -0300

    Model for list items and migration

--- tdd-python/project01/superlists/lists/tests.py
+++ tdd-python/project01/superlists/lists/tests.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.test import TestCase
 from django.urls import resolve
 
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -59,3 +60,22 @@ class HomePageTest(TestCase):
         expected_html = self._remove_csrf_token(expected_response)
 
         self.assertEqual(observed_html, expected_html)
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(second_saved_item.text, "Item the second")
