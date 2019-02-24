a1c8398e91588cc2d072493df117cf6a388a039d
Felipe V. Rodrigues <felipe@felipevr.com>
2019-02-24 14:59:46 -0300

    Add basic templating

--- tdd-python/project01/superlists/functional_tests/tests.py
+++ tdd-python/project01/superlists/functional_tests/tests.py
@@ -3,12 +3,12 @@ Functional tests for the Lists app
 """
 import time
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """
     Functional tests for a new user
     """
