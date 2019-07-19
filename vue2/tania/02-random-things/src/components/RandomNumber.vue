6aaf0c7c9cda1c0375ce52e34ea4b456487c9567
Felipe Rodrigues <felipe@felipevr.com>
2019-07-19 10:40:25 -0300

    Make Random Number component return more random-y numbers

--- vue2/tania/02-random-things/src/components/RandomNumber.vue
+++ vue2/tania/02-random-things/src/components/RandomNumber.vue
@@ -1,12 +1,17 @@
 <template>
     <div id="random-number">
-        <p>I like the number 7</p>
+        <p>I like the number {{ number }}</p>
     </div>
 </template>
 
 <script>
 export default {
-    name: "random-number"
+    name: "random-number",
+    data: function() {
+        return {
+            "number": Math.ceil(Math.random() * 1000)
+        }
+    }
 };
 </script>
 
