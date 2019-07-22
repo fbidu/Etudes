cb5e238c19af1750b34ee484c701e29214456afe
Felipe Rodrigues <felipe@felipevr.com>
2019-07-22 18:07:46 -0300

    Add basic validations and status messages

--- vue2/tania/02-random-things/src/components/UserForm.vue
+++ vue2/tania/02-random-things/src/components/UserForm.vue
@@ -1,10 +1,29 @@
 <template>
   <div id="user-form">
     <form @submit.prevent="handleSubmit">
+      <p v-if="error && submitting" class="error-message">
+        ! Please fill all the fields!
+      </p>
+      <p v-if="success" class="success-message">
+        ✅ Noice
+      </p>
       <label>E-Mail</label>
-      <input type="text" v-model="user.email"/>
+      <input
+        type="text"
+        v-model="user.email"
+        :class="{'has-error': submitting && invalidEmail}"
+        @focus="clearStatus"
+        @keypress="clearStatus"
+        ref="first"
+      />
       <label>Name</label>
-      <input type="text" v-model="user.name"/>
+      <input
+        type="text"
+        v-model="user.name"
+        :class="{'has-error': submitting && invalidName}"
+        @focus="clearStatus"
+        @keypress="clearStatus"
+      />
       <button>Add User</button>
     </form>
   </div>
@@ -14,20 +33,71 @@
 export default {
   name: "user-form",
   data() {
-      return {
-          user: {
-              email: "",
-              name: ""
-          }
+    return {
+      submitting: false,
+      error: false,
+      success: false,
+      user: {
+        email: "",
+        name: ""
       }
+    };
   },
+  computed: {
+    invalidName() {
+      return this.user.name === "";
+    },
+
+    invalidEmail() {
+      return this.user.email === "";
+    }
+  },
+
   methods: {
     handleSubmit() {
-      this.$emit("add:user", this.user)
+      this.submitting = true;
+      this.clearStatus();
+
+      if (this.invalidName || this.invalidEmail) {
+        this.error = true;
+        return;
+      }
+
+      this.$emit("add:user", this.user);
+      this.$refs.first.focus()
+
+      this.user = {
+        name: "",
+        email: ""
+      };
+
+      this.error = false;
+      this.success = true;
+      this.submitting = false;
+    },
+
+    clearStatus() {
+      this.success = false;
+      this.error = false;
     }
   }
 };
 </script>
 
 <style scoped>
+form {
+  margin-bottom: 2rem;
+}
+
+[class*="-message"] {
+  font-weight: 500;
+}
+
+.error-message {
+  color: #d33c40;
+}
+
+.success-message {
+  color: #32a95d;
+}
 </style>
