9a4d2e034588cb24ce6d05d1ef7f4af45fb0a80b
Felipe Rodrigues <felipe@felipevr.com>
2020-09-08 10:21:43 -0300

    Finish fb

--- elixir/fizz_buzz/lib/fizz_buzz.ex
+++ elixir/fizz_buzz/lib/fizz_buzz.ex
@@ -5,22 +5,24 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}) do
-    result
-    |> String.split(",")
-    |> Enum.map(&convert_and_evaluate/1)
+  defp handle_file_read({:ok, result}) do
+    result = result
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate/1)
+
+    {:ok, result}
   end
 
-  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
 
-  def convert_and_evaluate(elem) do
+  defp convert_and_evaluate(elem) do
     elem
     |> String.to_integer
     |> evaluate
   end
 
-  def evaluate(elem) when rem(elem, 15) == 0, do: :fizzbuzz 
-  def evaluate(elem) when rem(elem, 3) == 0, do: :fizz
-  def evaluate(elem) when rem(elem, 5) == 0, do: :buzz
-  def evaluate(elem), do: elem
+  defp evaluate(elem) when rem(elem, 15) == 0, do: :fizzbuzz
+  defp evaluate(elem) when rem(elem, 3) == 0, do: :fizz
+  defp evaluate(elem) when rem(elem, 5) == 0, do: :buzz
+  defp evaluate(elem), do: elem
 end
--- elixir/fizz_buzz/test/fizz_buzz_test.exs
+++ elixir/fizz_buzz/test/fizz_buzz_test.exs
@@ -1,8 +1,15 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns a correct converted list" do
+      expected = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuzz, :buzz]}
+      assert FizzBuzz.build("numbers.txt") == expected
+    end
+
+    test "when an invalid file is provided, returns a nice error" do
+      expected = {:error, "Error reading the file: enoent"}
+      assert FizzBuzz.build("invalid.txt") == expected
+    end
   end
 end
