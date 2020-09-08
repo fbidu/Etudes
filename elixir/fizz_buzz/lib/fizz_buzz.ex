4a8e321a8f53394a9e4cd12774175f1c40e47f6d
Felipe Rodrigues <felipe@felipevr.com>
2020-09-08 10:07:19 -0300

    Complete basic fb code

--- elixir/fizz_buzz/lib/fizz_buzz.ex
+++ elixir/fizz_buzz/lib/fizz_buzz.ex
@@ -5,6 +5,22 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}), do: result
-  def handle_file_read({:error, reason}), do: reason
+  def handle_file_read({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&convert_and_evaluate/1)
+  end
+
+  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  def convert_and_evaluate(elem) do
+    elem
+    |> String.to_integer
+    |> evaluate
+  end
+
+  def evaluate(elem) when rem(elem, 15) == 0, do: :fizzbuzz 
+  def evaluate(elem) when rem(elem, 3) == 0, do: :fizz
+  def evaluate(elem) when rem(elem, 5) == 0, do: :buzz
+  def evaluate(elem), do: elem
 end
