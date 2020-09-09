51db745554da3a95fd8c4ab27aaad56974fb0fb8
Felipe Rodrigues <felipe@felipevr.com>
2020-09-09 16:21:12 -0300

    Add game agent

--- elixir/ex_mon/lib/ex_mon.ex
+++ elixir/ex_mon/lib/ex_mon.ex
@@ -2,4 +2,10 @@ defmodule ExMon do
   def create_player(name, move_rnd, move_avg, move_heal) do
     ExMon.Player.build(name, move_rnd, move_avg, move_heal)
   end
+
+  def start_game(player) do
+    "Robotinikkkk"
+    |> create_player(:punch, :kink, :heal)
+    |> ExMon.Game.start(player)
+  end
 end
