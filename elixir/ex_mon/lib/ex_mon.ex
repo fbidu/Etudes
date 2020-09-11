13b8053c26193dbd42dd881a8655c326c4a9e99b
Felipe Rodrigues <felipe@felipevr.com>
2020-09-11 17:48:10 -0300

    Add move mechanics

--- elixir/ex_mon/lib/ex_mon.ex
+++ elixir/ex_mon/lib/ex_mon.ex
@@ -2,6 +2,7 @@ defmodule ExMon do
   @computer_name "Robotinikkkk"
 
   alias ExMon.{Game, Player, Status}
+  alias ExMon.Game.Actions
   def create_player(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
   end
@@ -12,5 +13,20 @@ defmodule ExMon do
     |> Game.start(player)
 
     Status.print_round_message()
+  end
+
+  def make_move(move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+  end
+
+  defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+
+  defp do_move({:ok, move}) do
+    case move do
+      :move_heal -> "realiza_cura"
+      move -> Actions.attack(move)
+    end
   end
 end
--- elixir/ex_mon/lib/status.ex
+++ elixir/ex_mon/lib/status.ex
@@ -5,4 +5,8 @@ defmodule ExMon.Status do
     IO.inspect(Game.info)
     IO.puts("---------------------")
   end
+
+  def print_wrong_move_message(move) do
+    IO.puts("=== Invalid Move #{move} ===")
+  end
 end
