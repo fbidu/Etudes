366e7f6bbc66d397bfa82bc4216bce2afc561f62
Felipe Rodrigues <felipe@felipevr.com>
2021-04-22 18:39:18 -0300

    Adiciona black, script de init e muda dockerfile pra compose

---
+++ Python/sqlalchemy/essential-sqlalchemy/init_db.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    MetaData,
+    Numeric,
+    String,
+    Table,
+)
+from sqlalchemy import create_engine
+
+engine = create_engine("postgresql+psycopg2://sqla:sqla@localhost:5433/sqla")
+engine.connect()
+metadata = MetaData()
+
+
+cookies = Table(
+    "cookies",
+    metadata,
+    Column("cookie_id", Integer(), primary_key=True),
+    Column("cookie_name", String(50), index=True),
+    Column("cookie_recipe_url", String(255)),
+    Column("cookie_sku", String(55)),
+    Column("quantity", Integer()),
+    Column("unit_cost", Numeric(12, 2)),
+)
+
+
+users = Table(
+    "users",
+    metadata,
+    Column("user_id", Integer(), primary_key=True),
+    Column("username", String(15), nullable=False, unique=True),
+    Column("email_address", String(255), nullable=False),
+    Column("phone", String(20), nullable=False),
+    Column("password", String(25), nullable=False),
+    Column("created_on", DateTime(), default=datetime.now),
+    Column("updated_on", DateTime(), default=datetime.now, onupdate=datetime.now),
+)
+
+orders = Table(
+    "orders",
+    metadata,
+    Column("order_id", Integer(), primary_key=True),
+    Column("user_id", ForeignKey("users.user_id")),
+)
+
+line_items = Table(
+    "line_items",
+    metadata,
+    Column("line_items_id", Integer(), primary_key=True),
+    Column("order_id", ForeignKey("orders.order_id")),
+    Column("cookie_id", ForeignKey("cookies.cookie_id")),
+    Column("quantity", Integer()),
+    Column("extended_cost", Numeric(12, 2)),
+)
+
+metadata.create_all(engine)
