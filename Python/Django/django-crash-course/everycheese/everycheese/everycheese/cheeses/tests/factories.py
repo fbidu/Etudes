1b570b9743da6f83fc4d0526887a6828b3bcac3a
Felipe Bidu Rodrigues <felipe@felipevr.com>
2021-06-19 09:57:09 -0300

    Update tests and factories to include country code

--- Python/Django/django-crash-course/everycheese/everycheese/everycheese/cheeses/tests/factories.py
+++ Python/Django/django-crash-course/everycheese/everycheese/everycheese/cheeses/tests/factories.py
@@ -11,6 +11,7 @@ class CheeseFactory(factory.django.DjangoModelFactory):
     slug = factory.LazyAttribute(lambda obj: slugify(obj.name))
     description = factory.Faker("paragraph", nb_sentences=3, variable_nb_sentences=True)
     firmness = factory.fuzzy.FuzzyChoice([x[0] for x in Cheese.Firmness.choices])
+    country_of_origin = factory.Faker("country_code")
 
     class Meta:
         model = Cheese
--- Python/Django/django-crash-course/everycheese/everycheese/everycheese/cheeses/tests/test_models.py
+++ Python/Django/django-crash-course/everycheese/everycheese/everycheese/cheeses/tests/test_models.py
@@ -2,11 +2,17 @@ import pytest
 
 pytestmark = pytest.mark.django_db
 
-from ..models import Cheese
 from .factories import CheeseFactory
+
 
 def test__str__():
     cheese = CheeseFactory()
 
     assert cheese.__str__() == cheese.name
     assert str(cheese) == cheese.name
+
+
+def test_cheese_has_country():
+    cheese = CheeseFactory()
+
+    assert cheese.country_of_origin
